39d5296219a4f516701eece24ace0d64bdad3d9c
Sai Naveen Bysani <sainaveenbysani@gmail.com>
2023-07-08 20:53:09 -0500

    HomePage Addition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home'
 
 function App() {
   return (
     <div className="App">
       <div className="mainSection">
-        <MainSection/>
+        <Home/>
       </div>
     </div>
   );
