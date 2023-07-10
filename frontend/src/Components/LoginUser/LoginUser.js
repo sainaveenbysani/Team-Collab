b8cae903d6106c9aec20ddce6201ec35f05d3873
Sai Naveen Bysani <sainaveenbysani@gmail.com>
2023-07-10 09:32:27 -0500

    Adding Login and register

---
+++ frontend/src/Components/LoginUser/LoginUser.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+
+const LoginUser= props =>{
+  const [enteredUsername, setUsername ] = useState('');
+  const [enteredPassword, setPassword ] = useState('');
+  const [loginMessage, setLoginMessage] = useState('');
+
+  const userNameChangeHandler = event =>{
+    setUsername(event.target.value);
+  }
+  const passwordChangeHandler = event =>{
+    setPassword(event.target.value);
+  }
+  const LoginMessage= event =>{
+    event.preventDefault();
+    setLoginMessage(`User ${enteredUsername} Logged in Successfully!!!`);
+    setUsername('');
+    setPassword('');
+  }
+  return (
+    <div>
+      <form className='user-login' onSubmit={LoginMessage}>
+      <h2>Login</h2>      
+      <label htmlFor="userName"><strong>Username:</strong></label>
+      <input type="text" id="userName" name="userName" value={enteredUsername} onChange={userNameChangeHandler} required /><br />
+      <label htmlFor="passWord"><strong>Password:</strong></label>
+      <input type="password" id="passWord" name="passWord" value={enteredPassword} onChange={passwordChangeHandler} required /><br />
+      <button type="submit">Login</button>
+    </form>{loginMessage&&
+      <p><strong>{loginMessage}</strong></p>
+    }
+    </div>
+    
+  );
+};
+
+
+export default LoginUser;
